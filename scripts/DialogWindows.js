fec090febaa96b5fb7148cf23c18c9036abecf82
Kamil Zieliński <kamil6395@gmail.com>
2022-05-16 21:42:16 +0200

    Merging

--- scripts/DialogWindows.js
+++ scripts/DialogWindows.js
@@ -30,14 +30,19 @@ function CreateTable(month, dialog){
     }
 
     dialog.innerHTML += `<button id="Close"></button>`
-    dialog.innerHTML += `<table id="monthView" class="${month}"><thead><tr><td id="Month">${month}</td></tr></thead><tbody id="${month}"></tbody></table>`
+    dialog.innerHTML += `<table id="monthView" class="${month}"><thead><tr><h1 id="Month">${month}</h1></tr></thead><tbody id="${month}"></tbody></table>`
     document.querySelector(`#${month}`).innerHTML += '<tr><td class="mon" id="1">Mon</td><td class="thu" id="2">Thu</td><td class="wed" id="3">Wed</td><td class="thur" id="4">Thur</td><td class="fri" id="5">Fri</td><td class="sat" id="6">Sat</td><td class="sun" id="7">Sun</td></tr>';
-
+    dialog.innerHTML+='<p id="Wydarzenie">Dodaj wydarzenie!</p>'
+    dialog.innerHTML+='<input type="text" id="eventName" required><br />'
+    dialog.innerHTML+='<input type="date" id="eventDate" required min="today"><br />'
+    dialog.innerHTML+='<input type="color" id="eventColor" required><br />'
+    dialog.innerHTML+='<input type="button" id="addEvent" value="Dodaj wydarzenie!"><br/>'
     week=0
-
+    newDate = new Date(year, monthNumber[month])
+    day = newDate.getDay();
     for(j=0;j != months[month];j++){
         if(j==0 || day ==1){
-            dialog.querySelector(`#${month}`).innerHTML += `<tr id=${month+week}></tr>`
+            dialog.querySelector(`#${month}`).innerHTML += `<tr style="margin-left:10px;"id=${month+week}></tr>`
             week++
         }
         if(day==7)
@@ -62,7 +67,7 @@ function CreateTable(month, dialog){
 
         if(j==0 || day==1)
             week++
-            dialog.querySelector(`#${month}${week-1}`).innerHTML += `<td id='${month}Day${j+1}'>${j+1}</td>`
+            dialog.querySelector(`#${month}${week-1}`).innerHTML += `<td style="text-align:right; margin-left:30px" id='${month}Day${j+1}'>${j+1}<div>*</div></td>`
         tempArray[month].push(days[day-1])
         if(day==7)
             day=0
